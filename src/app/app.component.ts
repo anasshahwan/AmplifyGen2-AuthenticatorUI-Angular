c89711ce0fdb51ba4aaa8dd3a12016ce70dd57ec
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-02 19:43:31 -0400

    authenticator UI added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AmplifyAuthenticatorModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'amplifyge2-authui';

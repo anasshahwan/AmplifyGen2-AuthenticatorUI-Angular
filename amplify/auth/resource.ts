3a282106bb828acfc68a10d7330a66f275983993
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-03 19:28:31 -0400

    Add Login With Google Provider

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,6 +7,19 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        scopes: ['profile', 'email'],
+        attributeMapping: {
+          email: 'email',
+          preferredUsername: 'name',
+        },
+      },
+      callbackUrls: ['http://localhost:4200', 'https://yourwebsite.com'],
+      logoutUrls: ['http://localhost:4200', 'https://yourwebsite.com'],
+    },
   },
   groups: ['Testers', 'Admins', 'Editors', 'Customers'],
   multifactor: {
